11df81926faf981223df6b8ea168fefeefdc99d3
Jared Lewis <jared@jaredlewis.net>
2014-01-10 14:59:44 +0900

    Added posts listing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,26 @@
-$(document).ready(function(){
+function equalizeHeights(items) {
+  var maxHeight = 0;
+  items.each(function() {
+    if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
+  });
+  items.each(function() { $(this).css('min-height',(maxHeight+10)+"px"); });
+}
+
+function formatPostListings() {
+  var posts = $('body.posts div.posts div.thumbnail');
+  if( $(window).width() >= 992 ) {
+    for(var i = 0; i < posts.length; i+=3) { equalizeHeights( posts.slice(i, i+3) ); }
+  } else if ( $(window).width() >= 768 ) {
+    for(var i = 0; i < posts.length; i+=2) { equalizeHeights( posts.slice(i, i+2) ); }
+  } else {
+    posts.each(function(){ $(this).css('min-height',"0px"); });
+  }
+}
+
+$(document).ready(function() {
+
+  $(window).on('load', formatPostListings);
+  $(window).on('resize', formatPostListings);
 
   // if they click on a boxed image, enlarge it
   $('div.img-box a').click(function(event) {
